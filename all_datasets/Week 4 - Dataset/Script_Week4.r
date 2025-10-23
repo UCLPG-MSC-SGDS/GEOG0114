f1c6610ea023bf7d2f8229b629b9faa6c77bd7d1
Anwar Musah <95010284+UCLPG-MSC-SGDS@users.noreply.github.com>
2025-10-23 14:27:17 +0100

    Major Updates to Week 4 Practical

--- all_datasets/Week 4 - Dataset/Script_Week4.r
+++ all_datasets/Week 4 - Dataset/Script_Week4.r
@@ -47,8 +47,8 @@ temp
 # all values > 0 and <= 15 change to 0
 # all values > 15 and <= 30 change to 1
 temp_cl <- c(
-	0, 15, 0, 
-	15, 30, 1
+	-Inf, 15, 0, 
+	15, Inf, 1
 	)
 
 # convert into a matrix format
@@ -73,8 +73,10 @@ elev
 
 # all values > -12 and <= 1199.999 change to 0
 # all values > 1199.999 and <= 4900 change to 1
-elev_cl <- c(-12, 1199.999, 1, 
-	1199.999, 4900, 0)
+elev_cl <- c(
+	-Inf, 1200, 1, 
+	1200, Inf, 0
+	)
 
 # convert into a matrix format
 elev_cl_mat <- matrix(elev_cl, ncol = 3, byrow = TRUE) 
@@ -94,8 +96,10 @@ tm_shape(elev_recl) +
 # nvdi
 nvdi
 
-nvdi_cl <- c(-1, 0.5, 0, 
-	0.5, 0.9, 1)
+nvdi_cl <- c(
+	-Inf, 0.5, 0, 
+	0.5, Inf, 1
+)
 
 nvdi_cl_mat <- matrix(nvdi_cl, ncol = 3, byrow = TRUE)
 nvdi_cl_mat
@@ -111,8 +115,10 @@ tm_shape(nvdi_recl) +
 # prec
 prec
 
-prec_cl <- c(0, 350, 0, 
-	350, 2700, 1)
+prec_cl <- c(
+	-Inf, 350, 0, 
+	350, Inf, 1
+)
 
 prec_cl_mat <- matrix(prec_cl, ncol = 3, byrow = TRUE) 
 prec_cl_mat
@@ -129,8 +135,10 @@ tm_shape(prec_recl) +
 # popl
 popl
 
-popl_cl <- c(-1, 0, 0, 
-	0, 126300, 1)
+popl_cl <- c(
+	-Inf, 0, 0, 
+	0, Inf, 1
+)
 
 popl_cl_mat <- matrix(popl_cl, ncol = 3, byrow = TRUE)
 popl_cl_mat
@@ -146,15 +154,17 @@ tm_shape(popl_recl) +
 # arid
 arid
 
-arid_cl <- c(0, 0.20, 0, 
-	0.20, 3, 1)
+arid_cl <- c(
+	-Inf, 0.20, 0, 
+	0.20, Inf, 1
+)
 
 arid_cl_mat <- matrix(arid_cl, ncol = 3, byrow = TRUE)
 arid_cl_mat
 arid_recl <- reclassify(arid, arid_cl_mat)
 
 tm_shape(arid_recl) + 
-	tm_raster(col.scale = tm_scale_categorical(values = c("grey", "orange"), labels = c("Unsuitable (0.2 & below)", "Suitable (> 0.2)")),
+	tm_raster(col.scale = tm_scale_categorical(values = c("grey", ""), labels = c("Unsuitable (0.2 & below)", "Suitable (> 0.2)")),
 		col.legend = tm_legend(title = "Aridity (Dryness)", frame = FALSE)) +
 	tm_shape(kenya_states) + 
 	tm_polygons(fill_alpha = 0, col = "black") +
@@ -182,7 +192,7 @@ Suitable_LF_Summed@data@max
 # maximum = 6
 
 tm_shape(Suitable_LF_Summed) + 
-	tm_raster(col.scale = tm_scale_categorical(values = c("#FDFEFE", "#FADBD8", "#F5B7B1", "#F1948A", "#E74C3C"), 
+	tm_raster(col.scale = tm_scale_categorical(values = c("#fff5f0", "#FADBD8", "#F5B7B1", "#F1948A", "#E74C3C"), 
 		labels=c("Low (2)", "Modest (3)", "Medium (4)", "High (5)", "Highest (6)")),
 		col.legend = tm_legend(title = "Suitability Map (Summation)", frame = FALSE)) +
 	tm_shape(kenya_states) + 
@@ -193,255 +203,94 @@ tm_shape(Suitable_LF_Summed) +
 
 # AHP
 
-# cleaning for temp
-# Extract values from Raster
-tempValues <- values(temp) 
+temp_standardised_10 <- ((temp - temp@data@min)/(temp@data@max-temp@data@min))*10
+plot(temp_standardised_10)
 
-# Change the values from vector object to data.frame object
-tempDF <- as.data.frame(tempValues)
+# repeat
+prec_standardised_10 <- ((prec - prec@data@min)/(prec@data@max-prec@data@min))*10
+plot(prec_standardised_10)
 
-# Remove missing values and reapply column name
-tempDF <- as.data.frame(tempDF[!is.na(tempDF$tempValues),])
-colnames(tempDF) <- "tempValues"
+popl_standardised_10 <- ((popl - popl@data@min)/(popl@data@max-popl@data@min))*10
+plot(popl_standardised_10)
 
-# Use the getJenksBreaks() function. Sample 0.10 (10%) of the pixels 
-#---at random and base the categorisation on this. 
-# NOTE: Doing this on the full data will take forever - so use the subset argument. 
-tempJenks <- getJenksBreaks(tempDF$tempValues, 10, subset = nrow(tempDF)*0.10)
+nvdi_standardised_10 <- ((nvdi - nvdi@data@min)/(nvdi@data@max-nvdi@data@min))*10
+plot(nvdi_standardised_10)
 
-# See value in vector
-tempJenks
-
-# shows first element
-tempJenks[1] 
-
-# shows second element
-tempJenks[2] 
-
-# so on and so further...
-# Create categorisation by using the Jenks values in the vector
-temp_jenks_cl <- c(temp@data@min-1, tempJenks[1], 1,
-	tempJenks[1], tempJenks[2], 2,
-	tempJenks[2], tempJenks[3], 3,
-	tempJenks[3], tempJenks[4], 4,
-	tempJenks[4], tempJenks[5], 5,
-	tempJenks[5], tempJenks[6], 6,
-	tempJenks[6], tempJenks[7], 7,
-	tempJenks[7], tempJenks[8], 8,
-	tempJenks[8], tempJenks[9], 9,
-	tempJenks[9], temp@data@max+1, 10) 
-
-# create matrix
-temp_jenks_cl_mat <- matrix(temp_jenks_cl, ncol = 3, byrow = TRUE)
-# view categorisation in matrix
-temp_jenks_cl_mat
-# reclassify original raster using the jenks classifications
-temp_jenks_recl <- reclassify(temp, temp_jenks_cl_mat)
-
-tm_shape(temp_jenks_recl) + tm_raster(style = "cont", title = "Temp (on Jenks scale)", palette= "-Spectral") +
-	tm_shape(kenya_states) + tm_polygons(alpha = 0, border.col = "black") +
-	tm_layout(frame = FALSE, legend.outside = TRUE)
-
-# For instance take the mean temperature for Kenya
-# Spectral colours are useful for diverging scales "-brewer.spectral" is Rd-Or-Yl-Gr-Bu. "-brewer.spectral" reverses the order
-tm_shape(temp_jenks_recl) + 
-	tm_raster(col.scale = tm_scale_continuous(values = "-brewer.spectral"),
-		col.legend = tm_legend(title = "Temperature [Rescaled (Jenks)]", frame = FALSE)) +
-	tm_shape(kenya_states) + 
-	tm_polygons(fill_alpha = 0, col = "black") +
-	tm_layout(frame = FALSE)
-
-# repeat for other variables
-
-# 2 prec
-# Extract values from Raster
-precValues <- values(prec) 
-# Change the values from vector object to data.frame object
-precDF <- as.data.frame(precValues)
-# Remove missing values and reapply column name
-precDF <- as.data.frame(precDF[!is.na(precDF$precValues),])
-colnames(precDF) <- "precValues"
-# Use the getJenksBreaks() function. Sample 0.10 (10%) of the pixels at random and base the categorisation on this. 
-# NOTE: Doing this on the full data will take forever - so use the subset argument. 
-precJenks <- getJenksBreaks(precDF$precValues, 10, subset = nrow(precDF)*0.10)
-# See value in vector
-precJenks
-# shows first element
-precJenks[1] 
-# shows second element
-precJenks[2] 
-# so on and so further...
-# Create categorisation by using the Jenks values in the vector
-prec_jenks_cl <- c(prec@data@min-1, precJenks[1], 1,
-	precJenks[1], precJenks[2], 2,
-	precJenks[2], precJenks[3], 3,
-	precJenks[3], precJenks[4], 4,
-	precJenks[4], precJenks[5], 5,
-	precJenks[5], precJenks[6], 6,
-	precJenks[6], precJenks[7], 7,
-	precJenks[7], precJenks[8], 8,
-	precJenks[8], precJenks[9], 9,
-	precJenks[9], prec@data@max+1, 10) 
-# create matrix
-prec_jenks_cl_mat <- matrix(prec_jenks_cl, ncol = 3, byrow = TRUE)
-# view categorisation in matrix
-prec_jenks_cl_mat
-# reclassify original raster using the jenks classifications
-prec_jenks_recl <- reclassify(prec, prec_jenks_cl_mat)
-
-
-# 3. popl
-# Extract values from Raster
-poplValues <- values(popl)
-# Change the values from vector object to data.frame object
-poplDF <- as.data.frame(poplValues)
-# Remove missing values and reapply column name
-poplDF <- as.data.frame(poplDF[!is.na(poplDF$poplValues),])
-colnames(poplDF) <- "poplValues"
-# Use the getJenksBreaks() function. Sample 0.10 (10%) of the pixels at random and base the categorisation on this. 
-# NOTE: Doing this on the full data will take forever - so use the subset argument. 
-poplJenks <- getJenksBreaks(poplDF$poplValues, 10, subset = nrow(poplDF)*0.10)
-# See value in vector
-poplJenks
-# shows first element
-poplJenks[1] 
-# shows second element
-poplJenks[2] 
-# so on and so further...
-# Create categorisation by using the Jenks values in the vector
-popl_jenks_cl <- c(popl@data@min-1, poplJenks[1], 1,
-	poplJenks[1], poplJenks[2], 2,
-	poplJenks[2], poplJenks[3], 3,
-	poplJenks[3], poplJenks[4], 4,
-	poplJenks[4], poplJenks[5], 5,
-	poplJenks[5], poplJenks[6], 6,
-	poplJenks[6], poplJenks[7], 7,
-	poplJenks[7], poplJenks[8], 8,
-	poplJenks[8], poplJenks[9], 9,
-	poplJenks[9], popl@data@max+1, 10) 
-# create matrix
-popl_jenks_cl_mat <- matrix(popl_jenks_cl, ncol = 3, byrow = TRUE)
-# view categorisation in matrix
-popl_jenks_cl_mat
-# reclassify original raster using the jenks classifications
-popl_jenks_recl <- reclassify(popl, popl_jenks_cl_mat)
-
-
-# 4 nvdi
-# Extract values from Raster
-nvdiValues <- values(nvdi) 
-# Change the values from vector object to data.frame object
-nvdiDF <- as.data.frame(nvdiValues)
-# Remove missing values and reapply column name
-nvdiDF <- as.data.frame(nvdiDF[!is.na(nvdiDF$nvdiValues),])
-colnames(nvdiDF) <- "nvdiValues"
-# Use the getJenksBreaks() function. Sample 0.10 (10%) of the pixels at random and base the categorisation on this. 
-# NOTE: Doing this on the full data will take forever - so use the subset argument. 
-# EXTRA NOTE: The values for nvdi are very close to each other and so the algorithm splits it to just two cateogries
-nvdiJenks <- getJenksBreaks(nvdiDF$tempValues, 2, subset = nrow(nvdiDF)*0.10)
-# See value in vector
-nvdiJenks
-# shows first element
-nvdiJenks[1] 
-# shows second element
-nvdiJenks[2] 
-# so on and so further...
-# Create categorisation by using the Jenks values in the vector
-nvdi_jenks_cl <- c(nvdi@data@min-1, nvdiJenks[1], 1,
-	nvdiJenks[1], nvdi@data@max+1, 2)
-# create matrix
-nvdi_jenks_cl_mat <- matrix(nvdi_jenks_cl, ncol = 3, byrow = TRUE)
-# view categorisation in matrix
-nvdi_jenks_cl_mat
-# reclassify original raster using the jenks classifications
-nvdi_jenks_recl <- reclassify(nvdi, nvdi_jenks_cl_mat)
-
-
-# 6. elev
-
-# Extract values from Raster
-elevValues <- values(elev) 
-# Change the values from vector object to data.frame object
-elevDF <- as.data.frame(elevValues)
-# Remove missing values and reapply column name
-elevDF <- as.data.frame(elevDF[!is.na(elevDF$elevValues),])
-colnames(elevDF) <- "elevValues"
-# Use the getJenksBreaks() function. Sample 0.10 (10%) of the pixels at random and base the categorisation on this. 
-# NOTE: Doing this on the full data will take forever - so use the subset argument. 
-elevJenks <- getJenksBreaks(elevDF$elevValues, 10, subset = nrow(elevDF)*0.10)
-# See value in vector
-elevJenks
-# shows first element
-elevJenks[1] 
-# shows second element
-elevJenks[2] 
-# so on and so further...
-# Create categorisation by using the Jenks values in the vector
-elev_jenks_cl <- c(elev@data@min-1, elevJenks[1], 1,
-	elevJenks[1], elevJenks[2], 2,
-	elevJenks[2], elevJenks[3], 3,
-	elevJenks[3], elevJenks[4], 4,
-	elevJenks[4], elevJenks[5], 5,
-	elevJenks[5], elevJenks[6], 6,
-	elevJenks[6], elevJenks[7], 7,
-	elevJenks[7], elevJenks[8], 8,
-	elevJenks[8], elevJenks[9], 9,
-	elevJenks[9], elev@data@max+1, 10) 
-# create matrix
-elev_jenks_cl_mat <- matrix(elev_jenks_cl, ncol = 3, byrow = TRUE)
-# view categorisation in matrix
-elev_jenks_cl_mat
-# reclassify original raster using the Jenks classifications
-elev_jenks_recl <- reclassify(elev, elev_jenks_cl_mat)
-
-# see plot in its original form
-plot(elev_jenks_recl)
 # Now flip the values by subtracting the values from the max value (here its 10)
-rev_elev_jenks_recl <- elev_jenks_recl
-values(rev_elev_jenks_recl) <- elev_jenks_recl@data@max - values(elev_jenks_recl)
-# see plot flipped/inverted
-plot(rev_elev_jenks_recl)
+plot(elev)
+elev_standardised_10 <- ((elev - elev@data@min)/(elev@data@max-elev@data@min))*10
+plot(elev_standardised_10)
 
-
-tm_shape(rev_elev_jenks_recl) + 
-	tm_raster(col.scale = tm_scale_continuous(values = "-brewer.spectral"),
-		col.legend = tm_legend(title = "Inverted Elev (on Jenks scale)", frame = FALSE)) +
-	tm_shape(kenya_states) + 
-	tm_polygons(fill_alpha = 0, col = "black") +
-	tm_layout(frame = FALSE)
+flipped_elev_standardised_10 <- elev_standardised_10
+values(flipped_elev_standardised_10) <- elev_standardised_10@data@max - values(elev_standardised_10)
+plot(flipped_elev_standardised_10)
 
 # use the rescaled columns in the formula
 suitablemap_WLC <- (0.372*prec_jenks_recl + 0.356*temp_jenks_recl + 0.159*popl_jenks_recl + 0.077*rev_elev_jenks_recl + 0.037*nvdi_jenks_recl) * arid_recl 
+
+suitablemap_WLC <- 0.372*prec_standardised_10 + 0.356*temp_standardised_10 + 0.159*popl_standardised_10 + 0.077*flipped_elev_standardised_10 + 0.037 * nvdi_standardised_10
+suitablemap_WLC
 # see plot
 plot(suitablemap_WLC)
 
+#| Component                      | Max Contribution (approx) |
+#| ------------------------------ | ------------------------- |
+#| 0.372 × 10 (precipitation)     | 3.72                      |
+#| 0.356 × 10 (temperature)       | 3.56                      |
+#| 0.159 × 10 (population)        | 1.59                      |
+#| 0.077 × 10 (flipped elevation) | 0.77                      |
+#| 0.037 × 10 (NDVI)              | 0.37                      |
+#| **Total Theoretical Max**      | **10.01**                 |
+	
+# Since your observed max ≈ 6.786, that’s perfectly logical — no pixel achieved the maximum 10 across all factors simultaneously, 
+# and some may have been reduced by the constraint layer.
+# So, 6.786 = highest achievable weighted suitability given your dataset.
 
-# create custom map by categorising the final raster output
-suitablemap_WLC_cl <- c(
-	-1, 1, 1,
-	1, 2, 2,
-	2, 3, 3,
-	3, 4, 4,
-	4, 5, 5,
-	5, 6, 6,
-	6, 7, 7,
-	7, 8, 8,
-	8, 10, 9)
+# re-standardised weighted suitability map, constrained by aridity layer
+SWLC_standardised_10 <- ((suitablemap_WLC - suitablemap_WLC@data@min)/(suitablemap_WLC@data@max-suitablemap_WLC@data@min))*10
+plot(SWLC_standardised_10)
 
-suitablemap_WLC_cl_mat <- matrix(suitablemap_WLC_cl, ncol = 3, byrow = TRUE)
-suitablemap_WLC_cl_recl <- reclassify(suitablemap_WLC, suitablemap_WLC_cl_mat)
+#| Range       | Category  |
+#| ----------- | --------- |
+#| 0.00 – 0.20 | Very Low  |
+#| 0.20 – 0.40 | Low       |
+#| 0.40 – 0.60 | Moderate  |
+#| 0.60 – 0.80 | High      |
+#| 0.80 – 1.00 | Very High |
+	
 
-suitablemap_WLC_cl_recl@data@min
-suitablemap_WLC_cl_recl@data@max
+SWLC_cl <- c(
+	-Inf,0,0,
+  0,2,1,
+	2,4,2, 
+	4,6,3, 
+	6,8,4, 
+	8,10,5
+	)
 
-table(suitablemap_WLC_cl_recl@data@values)
+SWLC_cl_mat <- matrix(SWLC_cl, ncol = 3, byrow = TRUE)
+SWLC_cl_mat
+SWLC_recl <- reclassify(SWLC_standardised_10, SWLC_cl_mat)
 
-tm_shape(suitablemap_WLC_cl_recl) + 
-	tm_raster(col.scale = tm_scale_categorical(values = "brewer.reds", 
-		labels = c("0", "4", "5", "6", "7", "8", "9")),
+table(SWLC_recl@data@values)
+
+Add the constraint layer
+# to filter only the values == 1
+arid_recl[arid_recl[] !=0 ] = NA
+
+# code for producing the final output
+tm_shape(SWLC_recl) + 
+	tm_raster(col.scale = tm_scale_categorical(values = c("white", "#74add1", "#e0f3f8", "#ffffbf", "#fee08b", "#f46d43"), 
+		labels = c("Not Suitable (0)", "Very Low (1)", "Low (2)", "Moderate (3)", "High (4)", "Very High (5)")),
 		col.legend = tm_legend(title = "Suitability Map (MCDA-AHP)", frame = FALSE)) +
+	tm_shape(arid_recl) + 
+	tm_raster(col.scale = tm_scale_categorical(values = "white"), 
+		col.legend = tm_legend(show = FALSE)) + 
 	tm_shape(kenya_states) + 
 	tm_polygons(fill_alpha = 0, col = "black") +
-	tm_text("NAME_1", size = "AREA") +
+	tm_text("NAME_1") +
 	tm_layout(frame = FALSE)
+		
 
+# to filter only the values == 1
+arid_recl[arid_recl[] !=0 ] = NA
